3022084cc64b1780602c9ebaa88072f5c5523670
Павло <pavlo.sad@gmail.com>
2020-11-03 16:08:19 +0200

    added styles and scores

--- app.js
+++ app.js
@@ -17,15 +17,71 @@ function toStr(x, y)
 {
    return `${x}${y}`;
 }
+let score; 
 
 function updateTable()
 {
+   score = document.getElementById('scores');
+   
    for(let x = 0; x < arr.length; x++)
    {
       for(let y = 0; y < arr[x].length; y++)
       {
             box = document.getElementById(toStr(x, y));
             box.innerHTML = arr[x][y].name;
+            switch(arr[x][y].name){
+               case '2':
+                  box.style.backgroundColor = 'grey'
+                  
+                  break;
+               case '4':
+                  box.style.backgroundColor = '#b3b3af'
+                  
+                  break;
+               case '8':
+                  box.style.backgroundColor = '#f5d33b'
+                  score.innerHTML = +score.innerHTML + 4;
+                  break;
+               case '16':
+                  box.style.backgroundColor = '#f0b324'
+                  score.innerHTML = +score.innerHTML + 8;
+                  break;
+               case '32':
+                  box.style.backgroundColor = '#d17a08'
+                  score.innerHTML = +score.innerHTML + 16;
+                  break;
+               case '64':
+                  box.style.backgroundColor = '#d16608'
+                  score.innerHTML = +score.innerHTML + 32;
+                  break;
+               case '128':
+                  box.style.backgroundColor = '#ad3d09'
+                  score.innerHTML = +score.innerHTML + 64;
+                  break;   
+               case '256':
+                  box.style.backgroundColor = '#ad2209'
+                  score.innerHTML = +score.innerHTML + 128;
+                  break;  
+               case '512':
+                  box.style.backgroundColor = '#6bad09'
+                  score.innerHTML = +score.innerHTML + 256;
+                  break;
+               case '1024':
+                  box.style.backgroundColor = '#17ad09'
+                  score.innerHTML = +score.innerHTML + 512;
+                  break; 
+               case '2048':
+                  box.style.backgroundColor = '#4386d9'
+                  score.innerHTML = +score.innerHTML + 124;
+               case '4096':
+                  box.style.backgroundColor = '#7743d9'
+                  score.innerHTML = +score.innerHTML + 2048;
+                  break;            
+
+               default:
+                  box.style.backgroundColor = 'white'
+                  break;
+            }
       }
    }
 }
@@ -56,6 +112,18 @@ y = array[r].y;
 box = document.getElementById('' + x + y);
 random = Math.floor(Math.random() * 2);
 random == 0 ? box.innerHTML = 2 : box.innerHTML = 4;
+switch(box.innerHTML)
+{
+   case '2':
+      box.style.backgroundColor = 'grey'
+      break;
+   case '4':
+      box.style.backgroundColor = '#b3b3af'
+      break;
+   default:
+      box.style.backgroundColor = 'white'
+      break;
+}
 a = box.innerHTML;
 arr[x][y].name = box.innerHTML;
 
